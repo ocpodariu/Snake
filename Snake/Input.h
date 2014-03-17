0f05bb34dd2e7232f41b61f146e0fe70c1d8a1a4
PODARIU Ovidiu <ocpodariu@yahoo.com>
2014-03-17 23:41:59 +0200

    Add Input.h.

---
+++ Snake/Input.h
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------
+*  Author:        Ovidiu PODARIU
+*  Date:          17-Mar-2014
+*
+*  It is used to get the input from the user.
+*
+*----------------------------------------------------------------*/
+
+#ifndef INPUT_H_INCLUDED
+#define INPUT_H_INCLUDED
+
+class Input {
+
+public:
+
+	char readInput();
+
+};
+
+#endif // INPUT_H_INCLUDED
