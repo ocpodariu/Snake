9825bedfecc2033a46261f774282f61785b2fec0
PODARIU Ovidiu <ocpodariu@yahoo.com>
2014-03-27 16:39:18 +0200

    Added copyright information.

--- Menu.cpp
+++ Menu.cpp
@@ -1,4 +1,24 @@
 /**----------------------------------------------------------------
+ *  Snake
+ *  Copyright (C) 2014  PODARIU Ovidiu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *----------------------------------------------------------------*/
+
+
+/**----------------------------------------------------------------
  *  Author:        Ovidiu PODARIU
  *  Date:          27-Mar-2014
  *
@@ -40,6 +60,10 @@
 
      displayOptions();
 
+     printf("\n\n");
+
+     displayCopyright();
+
  }
 
  void Menu::displayTitle() {
@@ -63,6 +87,12 @@
         printf(options[i].name);
         printf("\n\n");
      }
+
+ }
+
+ void Menu::displayCopyright() {
+
+     printf("\t     Copyright (C) 2014  PODARIU Ovidiu");
 
  }
 
