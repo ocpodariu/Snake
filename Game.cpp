6b4887c29bc256c5ddbca616e09a877c862dcbb2
PODARIU Ovidiu <ocpodariu@yahoo.com>
2014-03-27 12:40:54 +0200

    Included the main menu in the game.

--- Game.cpp
+++ Game.cpp
@@ -14,39 +14,93 @@
 #include "Output.h"
 #include "Snake.h"
 #include "Food.h"
+#include "Menu.h"
 
 int main() {
 
-	Board board;
-
 	Input input;
-	Output output(board);
 
-	Snake snake(board);
-	Food food(board);
+	Menu menu;
+
+	/**
+     * Has the following meaning:
+     *   - TRUE  -> the player wants to exit the game
+     *   - FALSE -> otherwise
+     */
+    boolean exit = false;
 
 	do {
-        snake.changeDirection(input.readInput());
 
-        snake.move();
+        do {
+            menu.changeOption(input.readInput());
+            menu.displayMenu();
+            Sleep(70);
+	    }
+	    while (menu.getLastCommand() != VK_RETURN);
 
-        // If the snake has eaten the food item
-        // generate a new one.
-        if (food.isEaten())
-            food.generateFood();
+        /**
+         * Depending on the selected option:
+         *   - New Game (0) -> a new game will be started
+         *   - Instructions (1) -> the instructions of the game
+         *                         will be displayed
+         *   - Exit (2) -> exit the game
+         */
+        switch (menu.getCurrentOption()) {
 
-        output.updateBoard();
+            case 0: {
 
-        Sleep(100);
-    }
-    while (snake.getLives());
+                Board board;
+                Output output(board);
+                Snake snake(board);
+                Food food(board);
 
-    output.showResults(snake.getScore());
+                do {
 
-    printf("Press any key to exit...\n");
-    while(true)
-        if (kbhit())
-            break;
+                    snake.changeDirection(input.readInput());
+
+                    snake.move();
+
+                    // If the snake has eaten the food item
+                    // generate a new one.
+                    if (food.isEaten())
+                        food.generateFood();
+
+                    output.updateBoard();
+
+                    Sleep(100);
+                }
+                while (snake.getLives());
+
+                output.showResults(snake.getScore());
+
+                printf("Press any key to return to the main menu...\n");
+                while(true)
+                    if (kbhit())
+                        break;
+
+                break;
+
+            }
+
+            case 1: {
+
+                menu.displayInstructions();
+
+                printf("\t Press any key to return to the main menu...");
+                while(true)
+                    if (kbhit())
+                        break;
+
+                break;
+
+            }
+
+            case 2: { exit = true; break; }
+
+        }
+
+	}
+	while (!exit);
 
     return 0;
 
